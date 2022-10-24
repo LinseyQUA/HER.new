624d4d596c70bdabf5b18972248d82747f22457d
LinseyQUA <112869671+LinseyQUA@users.noreply.github.com>
2022-10-24 13:11:32 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,6 +4,8 @@ console.log("hi");
 /*Elementen uit html ophalen*/
 var deButton = document.querySelector("img:nth-child(2)");
 var deNav = document.querySelector(".navigatie")
+var body = document.querySelector("body")
+
 
 /*functie koppelen aan knop als je klikt*/
 deButton.addEventListener("click", toggleMenu);
@@ -12,4 +14,16 @@ deButton.addEventListener("click", toggleMenu);
 /*verschijning van het hamburger menu*/
 function toggleMenu() {
     deNav.classList.toggle("toonMenu");
+  }
+
+
+
+
+/*Functie klikken op submit knop*/ 
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
+  
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
   }
